062c4f06dd6339cf0b9a82c3b068a4e42010ba92
Justin McNally <justin@kohactive.com>
2014-10-21 00:35:23 -0500

    tests

--- test/redis_queue_test.exs
+++ test/redis_queue_test.exs
@@ -6,11 +6,15 @@ defmodule Exq.RedisQueueTest do
   setup_all do
     :ets.new(:workers, [:named_table, :set, :public, {:read_concurrency, true}])
     TestRedis.setup
-    IO.puts "Start"
     on_exit fn ->
       TestRedis.teardown
     end
-  end 
+  end
+
+  setup do
+    TestRedis.flush_all
+    :ok
+  end
 
  
   test "enqueue/dequeue single queue" do
@@ -31,5 +35,30 @@ defmodule Exq.RedisQueueTest do
     {queue, _} = Exq.RedisQueue.dequeue(:testredis, "test", [{"default", 10}, {"myqueue", 5}])
     assert queue == :none
   end
+
+  test "should not dequeue a task when the max number of workers for a queue are running" do
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    :ets.insert(:workers, {"default", 5})
+    {queue, _} = Exq.RedisQueue.dequeue(:testredis, "test", [{"default", 5}])
+    assert queue == :none
+  end
+
+  test "should dequeue a task when the max number of workers for a queue are not running" do
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    Exq.RedisQueue.enqueue(:testredis, "test", "default", "MyWorker", [])
+    :ets.insert(:workers, {"default", 0})
+    {queue, _} = Exq.RedisQueue.dequeue(:testredis, "test", [{"default", 5}])
+    assert queue != :none
+  end
+
 end
 
